9ada3a2aa2492e426ac67d4981e3684e959b8f7d
Nikithayyadav <CHANDAVENANIKITHA9@GMAIL.COM>
2026-01-13 18:18:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Campus Guardian", layout="centered")
+
+st.title("🚨 Campus Guardian")
+st.write("AI-powered campus safety and emergency system")
+
+st.success("Your app is running!")
