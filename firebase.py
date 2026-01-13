8ba651a87b1d98e1d9465583ac57d7caa29eded7
Nikithayyadav <CHANDAVENANIKITHA9@GMAIL.COM>
2026-01-13 21:48:02 +0530

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
-cred = credentials.Certificate("firebase_key.json")
+cred = credentials.Certificate(r"C:\Users\acer\OneDrive\Desktop\campus-guardian-NextGen-coders\firebase_key.json")
+
 
 firebase_admin.initialize_app(cred, {
     "databaseURL": "https://YOUR_DATABASE_URL.firebaseio.com"
