60fbc13f89d2373b5ba2684250d4ed005521e792
Nikithayyadav <CHANDAVENANIKITHA9@GMAIL.COM>
2026-01-14 19:44:48 +0530

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,11 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import os
 
-cred = credentials.Certificate(r"C:\Users\acer\OneDrive\Desktop\campus-guardian-NextGen-coders\firebase_key.json")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+cred = credentials.Certificate(
+    os.path.join(BASE_DIR, "firebase_key.json")
+)
 
 firebase_admin.initialize_app(cred, {
-    "databaseURL": "https://YOUR_DATABASE_URL.firebaseio.com"
+    "databaseURL": "https://campus-guardian-1b2e3-default-rtdb.firebaseio.com"
 })
 
 ref = db.reference("sos_alerts")
